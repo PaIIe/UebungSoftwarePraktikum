42bf0f222ae33261ae720dac41095b07efac319e
Willy <Willy@Willy-PC.fritz.box>
2016-10-31 20:53:54 +0100

    Soweit fertig (Manipulation der SmartTennis werte noch..)

--- src/JavaTest/src/Abgastest/testpackage/Motor.java
+++ src/JavaTest/src/Abgastest/testpackage/Motor.java
@@ -7,9 +7,8 @@ public class Motor {
 	void CO2Ausstoﬂ(){
 		for (int j=0; j<5; j++)
 		{
-			System.out.printf("CO2-Ausstoﬂ: %f", ((drehzahl[j] * effizienz) / 10)); 
+			System.out.printf("CO2-Ausstoﬂ: %.2f g/km\n", ((drehzahl[j] * (1 - effizienz)) / 10)); 
 		}
-		
 	}
 	
 	void setDrehzahl(int[] Geschwindigkeit) // gehen von ganzahligen Geschwindigkeiten aus
@@ -19,7 +18,7 @@ public class Motor {
 			for (int i = 0; i < 5; i++)
 			{
 				drehzahl[i] = Geschwindigkeit[i] * 10;
-				System.out.printf("Geschwindigkeit : %d\n, Drehzahl: %d\n", Geschwindigkeit[i], drehzahl[i]);
+				System.out.printf("Geschwindigkeit : %d km/h, Drehzahl: %d 1/min\n", Geschwindigkeit[i], drehzahl[i]);
 			}
 		}
 		/*DEBUG*/
--- src/JavaTest/src/Abgastest/testpackage/Teststand.java
+++ src/JavaTest/src/Abgastest/testpackage/Teststand.java
@@ -7,7 +7,25 @@ public interface Teststand {
 		int[] speed = new int[]{20, 50, 70, 100, 120}; // Geschwindigkeiten Testwerte f¸r Motor
 		Motor motor = new Motor();
 		motor.setDrehzahl(speed);
-		motor.CO2Ausstoﬂ();
+		if (auto.getModell() == Modell.M6);
+		{
+			motor.setEff(0.7);
+			motor.CO2Ausstoﬂ();
+		}
+			
+		if (auto.getModell() == Modell.TENNIS);
+		{
+			motor.setEff(0.3);
+			motor.CO2Ausstoﬂ();
+		}
+			
+		if (auto.getModell() == Modell.SMARTTENNIS)
+		{
+			motor.setEff(0.9);
+			motor.CO2Ausstoﬂ();
+		}
+			
+		
 		
 	}
 	
@@ -26,8 +44,11 @@ public interface Teststand {
 		auto3.setFarbe(Color.BLUE);
 		auto3.setHersteller(Hersteller.IW);
 		auto3.setModell(Modell.SMARTTENNIS);
+		System.out.printf("\nTennis: \n");
 		performCO2Test(auto1);
+		System.out.printf("\nM6: \n");
 		performCO2Test(auto2);
+		System.out.printf("\nSmartTennis \n");
 		performCO2Test(auto3);
 		
 	}
