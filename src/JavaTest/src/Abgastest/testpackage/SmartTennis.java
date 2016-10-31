cd42d4666bad124634a70d73cf6058b12761f397
User <User@134.109.89.99>
2016-10-31 22:37:59 +0100

    von 0.6 auf 0.4 geändert

--- src/JavaTest/src/Abgastest/testpackage/SmartTennis.java
+++ src/JavaTest/src/Abgastest/testpackage/SmartTennis.java
@@ -3,7 +3,7 @@ package Abgastest.testpackage;
 public class SmartTennis extends Fahrzeug{
 	public void manipulateCO2Ausstoﬂ(double ausstoss)
 	{
-			System.out.printf("CO2-Ausstoﬂ: %.2f g/km\n", (ausstoss*0.6)); 
+			System.out.printf("CO2-Ausstoﬂ: %.2f g/km\n", (ausstoss*0.4)); 
 	}
 	
 }
